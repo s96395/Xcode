5222c855cb57c92aabd6d9f1ab145ea30075c7f0
s96395 <120694699+s96395@users.noreply.github.com>
2023-01-03 15:40:53 +0800

    Add files via upload

---
+++ RestaurantDetailView20221220/RestaurantDetailView20221220/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  List20221220
+//
+//  Created by USER on 2022/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    struct Restaurant : Identifiable{
+var id = UUID()
+var name: String
+var image: String
+}
+    
+struct BasicImageRow: View {
+var restaurant: Restaurant
+
+var body: some View {
+HStack {
+Image(restaurant.image)
+.resizable()
+.frame(width: 40, height: 40)
+.cornerRadius(5)
+Text(restaurant.name)
+}
+}
+}
+    
+    struct RestaurantDetailView: View {
+    var restaurant: Restaurant
+
+    var body: some View {
+    VStack {
+    Image(restaurant.image)
+    .resizable()
+    .aspectRatio(contentMode: .fit)
+
+    Text(restaurant.name)
+    .font(.system(.title, design: .rounded))
+    .fontWeight(.black)
+
+    Spacer()
+    }
+    }
+    }
+var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+Restaurant(name: "Homei", image: "homei"),
+Restaurant(name: "Teakha", image: "teakha"),
+Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+Restaurant(name: "Po's Atelier", image: "posatelier"),
+Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+Restaurant(name: "Homei", image: "upstate"),
+Restaurant(name: "Traif", image: "traif"),
+Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+Restaurant(name: "Five Leaves", image: "fiveleaves"),
+Restaurant(name: "Cafe Lore", image: "cafelore"),
+Restaurant(name: "Confessional", image: "confessional"),
+Restaurant(name: "Barrafina", image: "barrafina"),
+Restaurant(name: "Donostia", image: "donostia"),
+Restaurant(name: "Royal Oak", image: "royaloak"),
+Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+]
+    
+var body: some View {
+    NavigationView {
+    List {
+    ForEach(restaurants) { restaurant in
+        NavigationLink(destination:RestaurantDetailView(restaurant
+                                                            : restaurant)){
+            BasicImageRow(restaurant: restaurant)
+        }
+      }
+    }
+    .navigationBarTitle("Restaurants",displayMode: .inline)
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+static var previews: some View {
+ContentView()
+}
+}
